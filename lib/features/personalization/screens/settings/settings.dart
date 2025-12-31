ebfdffe1826834654d4d54beaef4e832f334fd5e
Sneha Kushwaha <snehakushwaha8757@gmail.com>
2025-12-31 18:37:55 +0530

    files updated

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,10 +1,12 @@
 import 'package:ecommerce_app/common/widgets/appbar/appbar.dart';
 import 'package:ecommerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecommerce_app/common/widgets/list_tiles/settings_menu_tile.dart';
 import 'package:ecommerce_app/common/widgets/list_tiles/user_profile_tile.dart';
 import 'package:ecommerce_app/common/widgets/texts/section_heading.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({
@@ -32,12 +34,52 @@ class SettingsScreen extends StatelessWidget {
             ),
 
             //---Body
-            const Padding(
-              padding: EdgeInsets.all(RSizes.defaultSpace),
+            Padding(
+              padding: const EdgeInsets.all(RSizes.defaultSpace),
               child: Column(
                 children: [
-                  RSectionHeading(title: 'Account Setting'),
-                  SizedBox(height: RSizes.spaceBtwItems,),
+                  const RSectionHeading(title: 'Account Settings', showActionButton: false),
+                  const SizedBox(height: RSizes.spaceBtwItems),
+
+                  const RSettingMenuTile(icon: Iconsax.safe_home, title: 'My Addresses', subTitle: 'Set shopping delivery address'),
+                  const RSettingMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subTitle: 'Add, remove products and move to checkout'),
+                  const RSettingMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subTitle: 'Pending and Completed Orders'),
+                  const RSettingMenuTile(icon: Iconsax.bank, title: 'Bank Account', subTitle: 'Withdraw balance to registered bank account'),
+                  const RSettingMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subTitle: 'List of all the discounted coupons'),
+                  const RSettingMenuTile(icon: Iconsax.notification, title: 'Notification', subTitle: 'Set any kind of notification message'),
+                  const RSettingMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subTitle: 'Manage data usage and connected accounts'),
+
+                   //---App Settings
+                  const SizedBox(height: RSizes.spaceBtwSections),
+                  const RSectionHeading(title: 'App Settings', showActionButton: true),
+                  const SizedBox(height: RSizes.spaceBtwItems),
+                  const RSettingMenuTile(icon: Iconsax.document_upload, title: 'Load data', subTitle: 'Upload Data to your Cloud Firebase'),
+                   RSettingMenuTile(
+                    icon: Iconsax.location, 
+                    title: 'Geolocation', 
+                    subTitle: 'Set recommendation based on location',
+                    trailing: Switch(value: true, onChanged: (value) {}),
+                    ),
+                    RSettingMenuTile(
+                    icon: Iconsax.security_user, 
+                    title: 'Safe Mode', 
+                    subTitle: 'Search result is safe for all ages',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                    ),
+                    RSettingMenuTile(
+                    icon: Iconsax.image, 
+                    title: 'HD Image Quality', 
+                    subTitle: 'Set image quality to be seen',
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                    ),
+
+                    //---Log out Button
+                    const SizedBox(height: RSizes.spaceBtwSections),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton(onPressed: (){}, child: const Text('Logout')),
+                    ),
+                    const SizedBox(height: RSizes.spaceBtwSections * 2.5),
                 ],
               ),
             ),
